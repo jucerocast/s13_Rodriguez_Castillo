ff998a19b35871ddc3d442aa91c2edf8da3acbef
jucerocast <92174149+jucerocast@users.noreply.github.com>
2021-11-05 11:28:31 -0600

    Create T1E1_pos_neg.py

---
+++ T1E1_pos_neg.py
@@ -0,0 +1,9 @@
+if __name__=='__main__':
+     print("Escribe un número entero: ")
+     n = int(input())
+     if n > 0:
+          print("Número positivo")
+     elif n < 0:
+          print("Número negativo")
+     else:
+          print("El número que ingresaste es 0")
