3881d304df1d878ec11bff7d567f6d6497da745e
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-12-09 15:57:02 -0600

    Update T1E2_even_odd.py

--- T1E2_even_odd.py
+++ T1E2_even_odd.py
@@ -1,4 +1,19 @@
-if __name__=='__main__':
+""""
+Date:     2021-11-05
+Authors:  Julio Cesar Rodriguez Castillo
+File:     T1E2_even_odd.py
+Brief:    Programa que determina si un numero entero es par o impar
+Score:    35
+Version:  0.1.1
+Fixes:    - EL código no realiza la función deseada
+
+          - PEP8 recomienda añadir espacios en blanco alrededor de
+               los operadores
+                
+          - No se incluyó directorio Tarea_01 en el repositorio
+"""
+
+if __name__=='__main__':                          # PEP8
      print("Escribe un número entero: ")
      n = int(input())
      if n > 0:
@@ -7,3 +22,4 @@ if __name__=='__main__':
           print("Número negativo")
      else:
           print("El número que ingresaste es 0")
+
