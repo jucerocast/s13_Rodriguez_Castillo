51f5ac1dbf745d94d1d3bb9ed01fcd0609174ef9
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-24 00:48:01 -0600

    Update T2E1_test_list.py

--- Tarea_02/T2E1_test_list.py
+++ Tarea_02/T2E1_test_list.py
@@ -1,3 +1,23 @@
+"""
+Date:       2021-11-17
+Authors:    Julio Cesar Rodriguez Castillo
+File:       
+Brief:      
+Score:      80
+Version:    1.1.1
+Fixes:      - Falto el encabezado (docstring)
+            
+            - PEP8 recomienda no rebasar los 99 carácteres en una línea 
+                de código, yo establecí en los requerimientos máximo 72
+                carácteres (aplica para comentarios también)
+            
+            - PEP8 recomienda añadir un espacio en blanco después del
+                carácter de coma ','
+            
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores, excepto en los paramétros
+"""
+
 if __name__ == '__main__':
     myList = ["Aristóteles", "Hegel", "Kant", "Kierkegaard", "Nietzsche", "Platón"]
 
