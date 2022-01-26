94fbeb7dbb670941a666d592948357e87dcc6320
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-25 18:53:10 -0600

    Update T3E1_return_test.py

--- Tarea_03/T3E1_return_test.py
+++ Tarea_03/T3E1_return_test.py
@@ -1,3 +1,22 @@
+"""
+Date:       2021-11-19
+Authors:    Julio Cesar Rodriguez Castillo
+File:       
+Brief:      
+Score:      45
+Version:    0.1.1
+Fixes:      - La funcion se debe declarar antes del la condicion
+                de if __name__ == '__main__':
+
+            - No puedes asignar a la variable numero el resultado
+                de print porque print no retorna nada
+
+            - [50] El programa no funciona
+            
+            - [5] No tiene docstring el archivo (encabezado)
+"""
+
+
 if __name__ == '__main__':
     numero = print(float(input("Escribe un número: ")))
 
